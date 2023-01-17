c5b02e855f139b79e2b1c77b29cdb9d549bfeb73
spirothXYZ <borcillofg2020@gmail.com>
2023-01-17 18:05:31 +0800

    Add project files.

---
+++ proyekgen/proyekgen.cpp
@@ -0,0 +1,12 @@
+﻿// proyekgen.cpp : Defines the entry point for the application.
+//
+
+#include "proyekgen.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
