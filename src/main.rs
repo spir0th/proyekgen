08c0e716f897512b48903a775b3796877af6d552
Feive Gian Borcillo <spir.0th.xyz@gmail.com>
2024-03-12 23:08:41 +0800

    feat: Add source files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    subcommand: Option<Subcommands>,
+    template: Option<String>
+}
+
+#[derive(Subcommand)]
+enum Subcommands {
+    Init {
+
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    if let Some(template) = cli.template.as_deref() {
+        println!("Value for template: {template}");
+    }
 }
